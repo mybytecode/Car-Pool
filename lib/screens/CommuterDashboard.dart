9e91c77dce36f15b81711b1fe2ed390562848af8
mybytecode <mybytecode@gmail.com>
2019-12-21 06:12:35 +0530

    Rider UI updated

--- lib/screens/CommuterDashboard.dart
+++ lib/screens/CommuterDashboard.dart
@@ -1,6 +1,9 @@
 
 
+import 'package:carpool/helper/CurrentLocation.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class CommuterDashBoard extends StatefulWidget
 {
@@ -10,13 +13,37 @@ class CommuterDashBoard extends StatefulWidget
 }
 class CommuterDashboardState extends State< CommuterDashBoard>
 {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("Commuter"),
-      ),
-    );
+  GoogleMapController mapController;
+
+  LatLng _center = LatLng(18.603090, 73.770450);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
   }
 
+  @override
+  Widget build(BuildContext context) {
+    getCurrentLocation();
+    return  Container(child: GoogleMap(
+      mapType: MapType.hybrid,
+      trafficEnabled: true,
+      scrollGesturesEnabled: true,
+      zoomGesturesEnabled: true,
+      myLocationEnabled: true,
+      myLocationButtonEnabled: true,
+      onMapCreated: _onMapCreated,
+      initialCameraPosition: CameraPosition(
+        tilt: 20.0,
+        zoom: 30.0,
+        target: _center,
+      ),
+    ),);
+  }
+
+  void getCurrentLocation()async {
+    LocationData data= await CurrentLocation().getCurrentLocation();
+    setState(() {
+      _center =LatLng( data.latitude,data.longitude);
+    });
+  }
 }
--- lib/screens/RiderDashboard.dart
+++ lib/screens/RiderDashboard.dart
@@ -1,7 +1,7 @@
-import 'package:carpool/helper/CurrentLocation.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class RiderDashBoard extends StatefulWidget {
   @override
@@ -9,37 +9,104 @@ class RiderDashBoard extends StatefulWidget {
 }
 
 class RiderDashboardState extends State<RiderDashBoard> {
-  GoogleMapController mapController;
-
-   LatLng _center = LatLng(18.603090, 73.770450);
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
+  DateTime dateTime = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
-    getCurrentLocation();
-    return  GoogleMap(
-          mapType: MapType.hybrid,
-          trafficEnabled: true,
-          scrollGesturesEnabled: true,
-          zoomGesturesEnabled: true,
-          myLocationEnabled: true,
-          myLocationButtonEnabled: true,
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            tilt: 20.0,
-            zoom: 30.0,
-            target: _center,
-          ),
-        );
-  }
-
-  void getCurrentLocation()async {
-    LocationData data= await CurrentLocation().getCurrentLocation();
-    setState(() {
-      _center =LatLng( data.latitude,data.longitude);
-    });
+    return Container(
+      color: Colors.black12,
+      padding: EdgeInsets.all(10),
+      child: Center(
+        child: ListView(
+          children: <Widget>[
+            const SizedBox(height: 10.0),
+            TextField(
+              onChanged: (value) {},
+              decoration: InputDecoration(
+                hintText: "Start location",
+                prefixIcon: Icon(
+                  FontAwesomeIcons.search,
+                  color: Colors.black54,
+                ),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            const SizedBox(height: 10.0),
+            Container(
+              child: TextField(
+                onChanged: (value) {},
+                decoration: InputDecoration(
+                  hintText: "Drop location",
+                  prefixIcon: Icon(
+                    FontAwesomeIcons.search,
+                    color: Colors.black54,
+                  ),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            RaisedButton(
+              padding: EdgeInsets.all(10),
+              onPressed: () {
+                DatePicker.showDateTimePicker(context,
+                    theme: DatePickerTheme(containerHeight: 200.0),
+                    showTitleActions: true,
+                    currentTime: dateTime, onConfirm: (date) {
+                  if (date.difference(dateTime).inMilliseconds < 0) {
+                    setState(() {
+                      dateTime = DateTime.now();
+                    });
+                    print(dateTime);
+                  } else {
+                    dateTime = DateTime.now();
+                  }
+                });
+              },
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    FontAwesomeIcons.solidCalendarAlt,
+                    color: Colors.black54,
+                  ),
+                  Container(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Text(dateTime.day.toString() +
+                          "-" +
+                          dateTime.month.toString() +
+                          "-" +
+                          dateTime.year.toString() +
+                          "  " +
+                          dateTime.hour.toString() +
+                          ":" +
+                          dateTime.minute.toString()))
+                ],
+              ),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(3)),
+              elevation: 4,
+            ),
+            const SizedBox(height: 20),
+            RaisedButton(
+                onPressed: () {},
+                color: Colors.pink,
+                textColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                elevation: 4,
+                padding: EdgeInsets.all(10),
+                child: Center(
+                  child: Center(
+                    child: Text(
+                      "Post a Ride..",
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ),
+                ))
+          ],
+        ),
+      ),
+    );
   }
 }
